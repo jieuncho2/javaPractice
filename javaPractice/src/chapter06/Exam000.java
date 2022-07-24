839219e261b29b5e426d4266efe631df29d88925
jieuncho2 <jieunchoi.developer@gmail.com>
2022-07-24 22:31:44 +0900

    220724

---
+++ javaPractice/src/chapter06/Exam000.java
@@ -0,0 +1,26 @@
+package chapter06;
+
+import java.util.Scanner;
+
+public class Exam000 {
+
+	static int max(int a, int b, int c) {
+		int max = a;
+		if(b > max) max = b;
+		if(c > max) max = c;
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 a: ");
+		int a = sc.nextInt();
+		System.out.print("정수 b: ");
+		int b = sc.nextInt();
+		System.out.print("정수 c: ");
+		int c = sc.nextInt();
+		System.out.println("최대값은 " + max(a,b,c) + "입니다.");
+	}
+
+}
