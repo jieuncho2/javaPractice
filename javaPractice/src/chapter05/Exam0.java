0633bc1b682c1b6db8daab6ef6b050980115cd43
jieuncho2 <jieunchoi.developer@gmail.com>
2022-07-24 22:56:22 +0900

    220724

---
+++ javaPractice/src/chapter05/Exam0.java
@@ -0,0 +1,32 @@
+package chapter05;
+
+import java.util.Scanner;
+
+public class Exam0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("좌하변이 직각인 정삼각형을 표시합니다.");
+		System.out.print("몇 단: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			putStars(i);
+			System.out.println();
+		}
+		
+	}
+
+	private static void putStars(int n) {
+		// TODO Auto-generated method stub
+		for(int j = n; j > n; j--) {
+			System.out.print(" ");
+		}
+		for(int j = 1; j <=n; j++) {
+			System.out.print("*");
+		}
+	}
+
+}
