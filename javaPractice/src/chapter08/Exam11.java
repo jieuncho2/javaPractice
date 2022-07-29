46b5852be07fcd2577240c32eb6aeadb4f0bc35d
jieuncho2 <jieunchoi.developer@gmail.com>
2022-07-29 12:43:39 +0900

    220729 inner class

--- javaPractice/src/chapter08/Exam11.java
+++ javaPractice/src/chapter08/Exam11.java
@@ -1,6 +1,7 @@
 package chapter08;
 
 class OuterClass {
+	int a = 3;
 	static int b = 4;
 
 	class Inner {
@@ -12,8 +13,8 @@ class OuterClass {
 	}
 
 	static class StaticInner {
-		public static String stat;
 		int d = 6;
+		static int stat = 10;
 
 		public static void staticMethod() {
 			System.out.println("<Static inner>");
@@ -24,25 +25,21 @@ class OuterClass {
 public class Exam11 {
 	public static void main(String[] args) {
 		OuterClass oc = new OuterClass();
-//		System.out.println("OuterClass의 a값 = " + oc.a);
-
+		System.out.println("OuterClass의 a값 = " + oc.a);
 		System.out.println("OuterClass의 b값 = " + oc.b);
 		System.out.println("OuterClass의 StaticInner의 stat값 = " + OuterClass.StaticInner.stat);
-//		System.out.println("OuterClass의 b값 = " + OuterClass.StaticInner.d); //접근 불가
+//		System.out.println("OuterClass의 d값 = " + OuterClass.StaticInner.d); //접근 불가
 
 		System.out.println("\n======= inner 클래스 접근하기 ========");
 		OuterClass oc2 = new OuterClass();
 		OuterClass.Inner i = oc2.new Inner(); //객체 생성
-
 		System.out.println("Inner의 c값 = " + i.c);
 		i.InnerMethod();
 
 		OuterClass.StaticInner si = new OuterClass.StaticInner(); //객체 생성
 		System.out.println("StaicInner의 d값 = " + si.d);
-
 //		객체를 사용해 접근
 		si.staticMethod();
-
 //		클래스명으로도 접근 가능
 		OuterClass.StaticInner.staticMethod();
 	}
