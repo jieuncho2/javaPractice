0260be6b363d1bb6c1bc34cea1944aecda8c3f08
jieuncho2 <jieunchoi.developer@gmail.com>
2022-08-08 09:04:43 +0900

    new

---
+++ javaPractice/src/chapter0/Exam10.java
@@ -0,0 +1,10 @@
+package chapter0;
+
+public class Exam10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaPractice/src/chapter0/Exam8.java
@@ -0,0 +1,10 @@
+package chapter0;
+
+public class Exam8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
