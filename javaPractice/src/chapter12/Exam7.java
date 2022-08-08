d40ae1b28b9789729cba69de3581fbb43654ff2c
jieuncho2 <jieunchoi.developer@gmail.com>
2022-08-08 11:53:05 +0900

    220808 Stack and Queue

--- javaPractice/src/chapter12/Exam7.java
+++ javaPractice/src/chapter12/Exam7.java
@@ -1,10 +1,36 @@
 package chapter12;
 
+import java.util.LinkedList;
+
+class MyQueue{
+	private LinkedList<String> arrayQueue = new LinkedList<String>();
+	
+	public void enQueue(String data) {
+		arrayQueue.add(data);
+	}
+	
+	public String deQueue() {
+		int len = arrayQueue.size();
+		if(len == 0) {
+			System.out.println("큐가 비었습니다.");
+			return null;
+		}
+		return (arrayQueue.remove(0));
+	}
+}
 public class Exam7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MyQueue queue = new MyQueue();
+		queue.enQueue("A");
+		queue.enQueue("B");
+		queue.enQueue("C");
 
+
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
+		System.out.println(queue.deQueue());
 	}
 
 }
--- javaPractice/src/chapter12/Exam8.java
+++ javaPractice/src/chapter12/Exam8.java
@@ -1,10 +1,43 @@
 package chapter12;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Message {
+	public String command;
+	public String to;
+
+	public Message(String command, String to) {
+		super();
+		this.command = command;
+		this.to = to;
+	}
+}
+
 public class Exam8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Queue<Message> messageQueue = new LinkedList<Message>();
+		
+		messageQueue.offer(new Message("sendMail", "홍길동"));
+		messageQueue.offer(new Message("sendSMS", "신용권"));
+		messageQueue.offer(new Message("sendKakaoTalk", "홍두께"));
+		
+		while(!messageQueue.isEmpty()) {
+			Message message = messageQueue.poll();
+			switch(message.command) {
+				case "sendMail":
+					System.out.println(message.to +"님에게 메일을 보냈습니다.");
+					break;
+				case "sendSMS":
+					System.out.println(message.to +"님에게 문자를 보냈습니다.");
+					break;
+				case "sendKakaoTalk":
+					System.out.println(message.to +"님에게 카카오톡을 보냈습니다.");
+					break;
+			}
+		}
 	}
 
 }
