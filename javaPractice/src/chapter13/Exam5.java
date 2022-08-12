76384faa8b17fa94bc3931635377076b0d9a6fe2
jieuncho2 <jieunchoi.developer@gmail.com>
2022-08-12 09:06:55 +0900

    new

---
+++ javaPractice/src/chapter13/Exam5.java
@@ -0,0 +1,10 @@
+package chapter13;
+
+public class Exam5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javaPractice/src/chapter13/Exam8.java
@@ -0,0 +1,10 @@
+package chapter13;
+
+public class Exam8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
